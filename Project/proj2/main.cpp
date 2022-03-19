44db2b708a9da2d83190adf4d6814af84c08e153
Qing <42001432+QingC0913@users.noreply.github.com>
2022-03-19 15:43:30 -0400

    Add files via upload

---
+++ Project/proj2/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * main.cpp
+ *
+ * COMP 15 Project 2: Six Degrees of Collaboration
+ * 
+ * by [NAME], [DATE]
+ */
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
