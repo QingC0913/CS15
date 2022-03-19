534e2b56b41dbc40ddf9b2162dea9a439d75c9ae
Qing <42001432+QingC0913@users.noreply.github.com>
2022-03-19 15:24:25 -0400

    Add files via upload

--- Lab/lab0/welcome.cpp
+++ Lab/lab0/welcome.cpp
@@ -1,1 +1,20 @@
+/* 
+ * Name: Qing Cheng 
+ * Date: 09/08/2021
+ * Lab 0
+ */
 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "Welcome to CS 15!" << endl;
+
+  // Add your new message here.
+  
+  cout << "QING's first program." << endl; 
+
+  return 0;
+}
